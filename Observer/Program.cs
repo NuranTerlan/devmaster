cda25e0a4866736599c4c3ddf73d45cf32a26980
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-07-28 18:09:26 +0400

    usage of Observer Pattern

---
+++ Observer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ds = new DataSource(new List<IObserver> {new Chart(), new SpreadSheet()});
+
+            for (int i = 1; i < 20; i += 2)
+            {
+                ds.AddValue(i);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
