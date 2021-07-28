2aa3e56e6f698b3e303f86b91963688e43130e84
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-07-28 18:09:04 +0400

    Main data source component is implemented

---
+++ Observer/DataSource.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Observer
+{
+    public class DataSource
+    {
+        public List<int> Values { get; private set; } = new List<int>();
+        private readonly List<IObserver> _observers;
+
+        public DataSource()
+        {
+        }
+        
+        public DataSource(List<IObserver> observers)
+        {
+            _observers = observers;
+        }
+
+        public void AddValue(int value)
+        {
+            Values.Add(value);
+            Console.WriteLine($"New value added to the ${nameof(DataSource)}: {value}");
+            NotifyObservers();
+        }
+
+        public void AttachObserver(IObserver newObs)
+        {
+            _observers.Add(newObs);
+        }
+
+        public void DetachObserver(IObserver removingObs)
+        {
+            _observers.Remove(removingObs);
+        }
+
+        public void NotifyObservers()
+        {
+            _observers.ToList().ForEach((o) => o.Reload(Values));
+        }
+    }
+}
