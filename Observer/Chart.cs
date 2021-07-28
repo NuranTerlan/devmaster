cf34d8262dfd29698d47135f81896fd770c8da7e
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-07-28 18:08:41 +0400

    Concrete Observer classes are created and inherited to implement desired observer behavior

---
+++ Observer/Chart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Observer
+{
+    public class Chart : IObserver
+    {
+        public void Reload(IEnumerable<int> values)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"From {nameof(Chart)}. Values are updated: {string.Join(", ", values)}");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Observer/SpreadSheet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Observer
+{
+    public class SpreadSheet : IObserver
+    {
+        public void Reload(IEnumerable<int> values)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"From {nameof(SpreadSheet)}. Sum changed: {values.Sum()}");
+            Console.ResetColor();
+        }
+    }
+}
